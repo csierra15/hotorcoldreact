a2e35bd6637eff529d2b5ac9aac28ba70822e3a7
Carly Sierra <carlylwomack@gmail.com>
2018-02-08 18:40:35 -0600

    listed actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,15 @@
+const MAKE_GUESS = 'MAKE_GUESS'
 export const makeGuess = (number) => ({
-  type: 'MAKE_GUESS',
+  type: MAKE_GUESS,
   number
+})
+
+const GET_FEEDBACK = 'GET_FEEDBACK'
+export const giveFeedback = () => ({
+  type: GET_FEEDBACK
+})
+
+const NEW_GAME = 'NEW_GAME'
+export const newGame = () => ({
+  type: NEW_GAME
 })
